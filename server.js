c2c05bab1f29c103192656819386d57cb6aebcde
Errolyn <Errolyn@mozilla.com>
2019-07-29 18:46:53 -0700

    added Handlebars got site basically functional

--- server.js
+++ server.js
@@ -1,41 +1,37 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-	
-app.use(express.static('public'));
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
 
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
-app.use('/views', express.static(__dirname + '/public/views'));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+app.get('/clicker', function (req, res) {
+    res.render('clicker');
+});
+app.get('/hangman', function (req, res) {
+    res.render('hangman');
+});
+app.get('/russian_roulette', function (req, res) {
+    res.render('russian_roulette');
+});
+app.get('/short_stories', function (req, res) {
+    res.render('short_stories');
+});
+app.get('/wordlookup', function (req, res) {
+    res.render('wordlookup');
+});
+
 
 var server = app.listen(8081, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
-
-app.get('/about', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/views/index.html'));
-})
-
-app.get('/clicker', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/views/clicker.html'));
-})
-
-app.get('/hangman', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/views/hangman.html'));
-})
-
-app.get('/russian_roulette', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/views/russian_roulette.html'));
-})
-
-app.get('/short_stories', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/views/short_stories.html'));
-})
-
-app.get('/wordlookup', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/views/wordlookup.html'));
-})
